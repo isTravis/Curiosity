c3896bc5f90552bd5f8a078fab1f381b582b3c2b
Travis Rich <travis.s.rich@gmail.com>
2014-02-17 09:57:54 -0500

    compareTopMillion.py updates

--- site/data/analysis/compareTopMillion.py
+++ site/data/analysis/compareTopMillion.py
@@ -1,14 +1,20 @@
-import pymongo
-import json
-import requests
+# import pymongo
+# import json
+# import requests
 import time
 import sys
-import cProfile
+import pickle
+# import cProfile
 
 
 # Load all links 
 # Iterate over and compare
 # create master object array that has {id:{id1:5,id7:13}}
+
+
+# pickle.dump( directPages_normalized, open( "../datasets/directPages_normalized.p", "wb" ) )
+# masterPageLinks = pickle.load(open("../datasets/masterPageLinks.p", "rb"))  
+
 
 def main ():
 	# def sharedLinkCount (list1, list2):
@@ -19,39 +25,46 @@ def main ():
 
 	######################################
 
-	connection = pymongo.Connection(host='127.0.0.1', port=3002)
-	try:
-		db = connection.meteor
-	except:
-		print "Can't seem to find the meteor database"
+	# connection = pymongo.Connection(host='127.0.0.1', port=3002)
+	# try:
+	# 	db = connection.meteor
+	# except:
+	# 	print "Can't seem to find the meteor database"
 
-	c_wikiLinks = db.wikilinks
-	c_wikiStrengths = db.wikistrengths
+	# c_wikiLinks = db.wikilinks
+	# c_wikiStrengths = db.wikistrengths
 
-	allLinks = {}
-	allIDs = set()
+	# allLinks = {}
+	# allIDs = set()
+	# allResults = []
+
+	# print "Loading Pages"
+	# linksDB = c_wikiLinks.find()
+	# for page in linksDB:
+	# 	pageID =  page['pageID']
+	# 	pageLinks = set(page['links'])
+	# 	allLinks[pageID] = pageLinks
+	# 	allIDs.add(pageID)
+	# linksDB = None # Clear the links from memory
+	# print "Loaded # Pages: " + str(len(allIDs))
+	# pickle.dump( allLinks, open( "allLinks.p", "wb" ) )
+	# pickle.dump( allIDs, open( "allIDs.p", "wb" ) )
+
+	print "Loading Pickles"
+	allLinks = pickle.load(open( "allLinks.p", "rb" ))  
+	allIDs = pickle.load(open( "allIDs.p", "rb" ))  
 	allResults = []
-
-	print "Loading Pages"
-	linksDB = c_wikiLinks.find()
-	for page in linksDB:
-		pageID =  page['pageID']
-		pageLinks = set(page['links'])
-		allLinks[pageID] = pageLinks
-		allIDs.add(pageID)
-	linksDB = None # Clear the links from memory
-	print "Loaded # Pages: " + str(len(allIDs))
-
+	print "Loaded Page Count: " + str(len(allIDs))
 
 	print "Beginning Links Calculation"
-	totalPages = 100000 
+	totalPages = 50000 
 	# totalPages = 2000
 	startTime = time.time()
 	counter = 0
 	finishedPages = 0
 	taskNum = int(sys.argv[1]) - 1
 	for pageID1 in allIDs:
-		if counter > ((taskNum*100000)-1) and counter < ((taskNum+1)*100000):
+		if counter > ((taskNum*50000)-1) and counter < ((taskNum+1)*50000):
 			connections = {}
 			list1 = allLinks[pageID1]
 			for pageID2 in allIDs:
@@ -70,8 +83,11 @@ def main ():
 
 		counter += 1
 		
-	print "Adding to mongo..."
-	c_wikiStrengths.insert(allResults)
+	# print "Adding to mongo..."
+	# c_wikiStrengths.insert(allResults)
+	# print "Done!"
+	print "Dumping Pickle..."
+	pickle.dump( allResults, open( "allResults_" + str(sys.argv[1]) + ".p", "wb" ) )
 	print "Done!"
 
 main()
